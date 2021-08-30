6c1cbcfe051d41352c3a98bb81edfbca28855b37
koksalkaradas <karadas.koksal@gmail.com>
2021-08-30 16:58:49 +0300

    fixed db connection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS CUSTOMER;
 
-create table customer (
+create table CUSTOMER (
     customer_num bigint not null,
     city varchar(255),
     district varchar(255),
@@ -9,7 +9,7 @@ create table customer (
     primary key (customer_num)
 );
 
-INSERT INTO customer (customer_num, name, surname, city, district) VALUES
+INSERT INTO CUSTOMER (customer_num, name, surname, city, district) VALUES
   (1, 'Adam', 'Avery', 'Maryland', 'Baltimore'),
   (2, 'Joseph', 'Bower', 'Nebraska', 'Lincoln'),
   (3, 'Liam', 'Campbell', 'New Jersey', 'Trenton');
